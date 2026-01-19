77a1dfcf02cd4bc5f1187bba2e4858cb4012e232
ubernaut <collin.schroeder@gmail.com>
2026-01-18 22:48:37 -0900

    fixed router

--- bindings/javascript/demo/src/main.tsx
+++ bindings/javascript/demo/src/main.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { App } from './App';
 import './styles/index.css';
 
-const basePath =
-  typeof document !== 'undefined'
-    ? new URL(document.baseURI).pathname
-    : import.meta.env.BASE_URL;
-const baseDir = basePath.endsWith('/') ? basePath : basePath.replace(/\/[^/]*$/, '/');
-const routerBase = baseDir.replace(/\/$/, '') || '/';
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename={routerBase}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
